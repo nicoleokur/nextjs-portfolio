1d0bbdaf62e87ac438fc6b05fc579eeb052a969b
nicoleokur <nokur@villanova.edu>
2025-12-18 15:53:59 -0500

    changed blog titles and dates to when i joined each club

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`  Hi! I am Nicole Okur. I am currently a junior student at Villanova University. 
+        {`Hi! I am Nicole Okur. I am currently a junior student at Villanova University. 
           I am majoring in computer engineering and minoring in computer science and electrical
           engineering. I enjoy getting involved on campus in clubs such as IEEE, Chi Omega, and 
           the Society of Women Engineers. Thank you for visiting my website!`}
