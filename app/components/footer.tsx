4c3c80c579efb74d47bab5ff4b5183a61d99c25d
nicoleokur <nokur@villanova.edu>
2025-12-18 15:46:05 -0500

    added a link to my linkedin and changed github link to be mine

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/nicoleokur/nextjs-portfolio"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -48,8 +48,19 @@ export default function Footer() {
             target="_blank"
             href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
-            <ArrowIcon />
+        <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
+            </a>
+           </li>
+           <li>
+             <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/nicole-okur-569508297/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">LinkedIn</p>
           </a>
         </li>
       </ul>
